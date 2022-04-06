aed7438577c104b4ecae3e3e1a42dfdb611c9be1
chaoqiang.lou <chaoqiang.l@renrenbit.com>
2022-04-06 12:04:45 +0800

    header over

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
+import { config } from 'process'
+import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
+import { buildURL } from './helpers/url'
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig) {
+  processConfig(config)
   xhr(config)
+}
+
+/** 处理config */
+function processConfig(config: AxiosRequestConfig) {
+  config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
+}
+
+function transformUrl(config: AxiosRequestConfig) {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+/** 转换请求 body 的数据 */
+function transformRequestData(config: AxiosRequestConfig) {
+  return transformRequest(config.data)
+}
+
+// 处理header
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,3 @@
-export interface AxiosRequestConfig {
-  url: string
-  method?: Method
-  data?: any
-  params?: any
-}
-
 export type Method =
   | 'get'
   | 'GET'
@@ -20,3 +13,23 @@ export type Method =
   | 'PUT'
   | 'patch'
   | 'PATCH'
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+  headers?: any
+  responseType?: XMLHttpRequestResponseType
+}
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+// 当axios 返回的是 AxiosPromise 类型，那么 resolve 函数中的参数就是一个 AxiosResponse 类型
+export interface AxiosPromise extends Promise<AxiosResponse> {}
